c416667ba8895563e401bcd77cb4c135b6d89456
mbartkowiak <michael.bartkowiak@gmail.com>
2016-05-30 23:15:47 -0500

    loops

---
+++ Javascript/loops.js
@@ -0,0 +1,15 @@
+var count = 0;
+do{
+  console.log(count);
+  count+=1;
+}while (count <1);
+
+
+for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
+
+while(count < 1){
+  console.log(count);
+  count+=1;
+}
