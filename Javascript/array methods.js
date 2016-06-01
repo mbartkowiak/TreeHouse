321a7da7f82df3fa030175af11bb04ef214b0c3b
mbartkowiak <michael.bartkowiak@gmail.com>
2016-05-31 23:01:43 -0500

    array work

---
+++ Javascript/array methods.js
@@ -0,0 +1,5 @@
+//array methods
+//push - pushes a value into the end of an array
+//pop - pops the last value out of the array and returns the value
+//shift - removes the first value out of the array and returns the value
+//unshift - adds a new value to the beginning of an array
